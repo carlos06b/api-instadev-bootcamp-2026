9e24faaa425f2f7299f0d2abbaf220d59aeb701e
Carlos Junior <carloslaujunior23@gmail.com>
2026-09-03 21:31:48 -0300

    feat: adiciona atualização de post e padroniza rotas de post

--- src/apps/controllers/PostController.js
+++ src/apps/controllers/PostController.js
@@ -1,10 +1,10 @@
-const Post = require('../models/Posts');
+const Posts = require('../models/Posts');
 
 class PostController {
   async create(req, res) {
     const { image, description } = req.body;
 
-    const newPost = await Post.create({
+    const newPost = await Posts.create({
       image,
       description,
       author_id: req.userID,
@@ -20,7 +20,7 @@ class PostController {
   async delete(req, res) {
     const { id } = req.params;
 
-    const verifyPost = await Post.findOne({ where: { id } });
+    const verifyPost = await Posts.findOne({ where: { id } });
 
     if (!verifyPost) {
       return res.status(404).json({ message: 'Post não encontrado' });
@@ -30,13 +30,35 @@ class PostController {
       return res.status(401).json({ message: 'Não autorizado' });
     }
 
-    const deletedPost = await Post.destroy({ where: { id } });
+    const deletedPost = await Posts.destroy({ where: { id } });
 
     if (!deletedPost) {
       return res.status(400).json({ message: 'Erro ao deletar o post' });
     }
 
     return res.status(200).json({ message: 'Post deletado com sucesso' });
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const verifyPost = await Posts.findOne({ where: { id } });
+
+    if (!verifyPost) {
+      return res.status(404).json({ message: 'Post não encontrado' });
+    }
+
+    if (verifyPost.author_id !== req.userID) {
+      return res.status(401).json({ message: 'Não autorizado' });
+    }
+
+    const postUpdate = await Posts.update(req.body, { where: { id } });
+
+    if (!postUpdate) {
+      return res.status(400).json({ message: 'Erro ao atualizar o post' });
+    }
+
+    return res.status(200).json({ message: 'Post atualizado com sucesso' });
   }
 }
 
